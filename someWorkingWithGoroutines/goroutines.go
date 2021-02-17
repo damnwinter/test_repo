ff9a922373fdc2491a363a030753b3ec54bf5494
damnwinter <79164212+damnwinter@users.noreply.github.com>
2021-02-17 22:44:20 +0300

    Simple example working with goroutines
    Parsing some sites for getting coin values

---
+++ someWorkingWithGoroutines/goroutines.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+type coin struct {
+	Name string
+	CurrentValue []byte
+	YesterdayVal []byte
+}
+
+func (c coin) String() string {
+	return fmt.Sprintf("%s:\n  Yesterday:  %s\n  Today:      %s", c.Name, c.YesterdayVal, c.CurrentValue)
+}
+
+func main() {
+
+	url := "https://cbr.ru"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	startIndex := bytes.Index(body, []byte("main-indicator_rate"))
+	eurIndex := bytes.Index(body[startIndex:], []byte("EUR"))
+	endIndex := bytes.Index(body[startIndex + eurIndex:], []byte("Официальный курс Банка России"))
+
+	reg, err := regexp.Compile("\\d+,\\d+")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	matches := reg.FindAll(body[startIndex : startIndex + eurIndex + endIndex], 4)
+	if len(matches) != 4 {
+		fmt.Println("Something went wrong, abort...")
+		return
+	}
+	usb := coin{
+		Name: "USB",
+		YesterdayVal: matches[0],
+		CurrentValue: matches[1],
+	}
+	fmt.Println(usb)
+
+	return
+}
