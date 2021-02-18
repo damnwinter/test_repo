33cfe68c91fad6207eec7b24ffb648615b931364
damnwinter <79164212+damnwinter@users.noreply.github.com>
2021-02-18 23:49:38 +0300

    Parse all coins
    Create function for parsing all coins from CB site

--- someWorkingWithGoroutines/goroutines.go
+++ someWorkingWithGoroutines/goroutines.go
@@ -18,13 +18,60 @@ func (c coin) String() string {
 	return fmt.Sprintf("%s:\n  Yesterday:  %s\n  Today:      %s\n", c.Name, c.YesterdayVal, c.CurrentValue)
 }
 
+type coinV2 struct {
+	intCode int
+	strCode string
+	Name string
+	Value float32
+}
+
+
 func main() {
+
+	coins, err := GetAllCoins()
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, coin := range coins {
+		fmt.Println(coin)
+	}
+
+	return
+}
+
+func GetAllCoins() ([]coin, error) {
+	url := "https://cbr.ru/currency_base/daily/"
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	//fmt.Println(string(body))
+
+	start := "<table class=\"data\">"
+	end := "</table>"
+	ind_start := bytes.Index(body, []byte(start))
+	ind_end := bytes.Index(body[ind_start:], []byte(end))
+	fmt.Println(string(body[ind_start : ind_start + ind_end + 8]))
+	
+
+	return nil, nil
+
+}
+
+
+func GetCoin() ([]coin, error) {
 
 	url := "https://cbr.ru"
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil, nil
 	}
 	defer resp.Body.Close()
 
@@ -36,25 +83,25 @@ func main() {
 	reg, err := regexp.Compile("\\d+,\\d+")
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil, nil
 	}
 	matches := reg.FindAll(body[startIndex : startIndex + eurIndex + endIndex], 4)
 	if len(matches) != 4 {
 		fmt.Println("Something went wrong, abort...")
-		return
+		return nil, nil
 	}
-	usb := coin{
+
+	coins := make([]coin, 2)
+
+	coins[0] = coin{
 		Name: "USB",
 		YesterdayVal: matches[0],
 		CurrentValue: matches[1],
 	}
-	eur := coin{
+	coins[1] = coin{
 		Name: "EUR",
 		YesterdayVal: matches[2],
 		CurrentValue: matches[3],
 	}
-	fmt.Println(usb)
-	fmt.Println(eur)
-
-	return
+	return coins, nil
 }
