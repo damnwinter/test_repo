4ce92cf59c88c3ee8754b5aee185e23a689f4773
damnwinter <79164212+damnwinter@users.noreply.github.com>
2021-02-24 23:37:58 +0300

    Update goroutines_examples.go

--- someWorkingWithGoroutines/goroutines_examples.go
+++ someWorkingWithGoroutines/goroutines_examples.go
@@ -20,33 +20,8 @@ func main() {
 	fmt.Println("Or this one too!")
 	anotherRight()
 
-
-	fmt.Println("Worker pool")
-	workerool()
 }
 
-func workerool() {
-
-	goroutinesNum := 20
-	ch := make(chan struct{}, goroutinesNum)
-
-	// Some very big value
-	allWorks := int32(10000000)
-
-	var ind int32
-	for ind = 0; ind < allWorks; {
-		go func() {
-			ch <- struct{}{}
-			// some work for worker
-
-
-
-			<- ch
-		}()
-	}
-	fmt.Println(ind)
-
-}
 
 func wrongUse() {
 
