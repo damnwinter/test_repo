851666508026b05cb315ec1689d31db155a771d9
damnwinter <79164212+damnwinter@users.noreply.github.com>
2021-02-25 00:29:04 +0300

    Update workerpool.go

--- someWorkingWithGoroutines/workerpool.go
+++ someWorkingWithGoroutines/workerpool.go
@@ -10,18 +10,56 @@ func main() {
 	fmt.Println("Worker pool 1")
 	workerPoolAllFixed()
 
-
-
 	fmt.Println("Worker pool 2")
+	workerPoolAnyJobsFixedWorker(4)
+}
 
 
+// worker live, while execute jobs (one worker -> done only one job)
+func workerPoolAnyJobsFixedWorker(workerCount int) {
+
+	jobsCount := 100
+
+	jobs := make(chan int, workerCount)
+	jobsOut := make(chan int, workerCount)
+
+	go func() {
+		for i := 0; i < jobsCount; i++ {
+			jobs <- i
+		}
+	}()
+
+	wg := sync.WaitGroup{}
+	go func() {
+		for i := 0; i < jobsCount; i++ {
+			wg.Add(1)
+			go workerAnyJobsFixedWorker(i + 1, jobs, jobsOut, &wg)
+		}
+		wg.Wait()
+		close(jobs)
+		close(jobsOut)
+	}()
+
+	for out := range jobsOut {
+		fmt.Printf("Jobs %d output\n", out)
+	}
 
 }
 
+func workerAnyJobsFixedWorker(id int, jobs <-chan int, jobsOut chan<- int, group *sync.WaitGroup) {
+	defer group.Done()
+	fmt.Printf("Worker %d started\n", id)
+
+	job := <- jobs
+	jobsOut <- job
+	fmt.Printf("Worker %d finished\n", id)
+}
+
+
+
+
 // Fixed worker count, fixed jobs (all started know)
 func workerPoolAllFixed() {
-
-
 	jobsCount := 10
 	jobs := make(chan int, jobsCount)
 	jobsOut := make(chan int, jobsCount)
