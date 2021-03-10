284bc3209f53e7989824217d1f46577b98835b7d
damnwinter <79164212+damnwinter@users.noreply.github.com>
2021-03-10 13:11:40 +0300

    Update and rename tests.go to main_test.go

---
+++ tests_examples/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+
+type testData struct {
+  values []int
+  sum int
+}
+
+dataForTest := []testData{
+  {[1, 2, 3], 6},
+  {[5, -5, 5], 5},
+}
+
+
+
+func TestSum(t *testing.T) {
+  for _, oneTest := range dataForTest {
+    funcSum := Sum(oneTest.values)
+    if funcSum != oneTest.sum {
+      //some error log
+    }
+  }
+
+}
--- tests_examples/tests.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "testing"
-
-func TestlastDirSearch(t *testing) {
-
-}
