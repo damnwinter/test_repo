d831ee336d03329d9c20424f4e81961f2b03d4b9
damnwinter <79164212+damnwinter@users.noreply.github.com>
2021-03-11 21:57:52 +0300

    Update main.go

--- tests_examples/main.go
+++ tests_examples/main.go
@@ -1,6 +1,17 @@
 
 package main
+import "fmt"
+
+func Sum(data []int) int {
+  s := 0
+  for _, val := range data {
+    s += val
+  }
+  return s
+}
+
 
 func main() {
-
+  data := []int{ 1, 2, 4, -7}
+  fmt.Println(Sum(data))
 }
